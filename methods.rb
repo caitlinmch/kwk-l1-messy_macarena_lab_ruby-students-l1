619f7c324d968ead6807d3777e5758f738e004ac
caitlinmch <caitlinmch@me.com>
2018-06-04 23:09:08 +0000

    Done.

---
+++ methods.rb
@@ -0,0 +1,21 @@
+def about_me
+  puts "My name is Caitlin"
+  puts "I grew up in Michigan and Arizona"
+  puts "I am 15"
+  puts "My favorite food is fries"
+end
+about_me
+
+def greeting 
+local_greeting = "Hi"
+  your_name = "Karlie"
+  my_name = "Caitlin"
+ time_of_day = "afternoon"
+ puts "#{local_greeting} #{your_name}, i'm #{my_name}, how's your #{time_of_day}"
+end
+greeting 
+
+  
+
+  
+  
---
+++ whats_your_name.rb
@@ -0,0 +1,5 @@
+puts "What is your name"
+#print what is your name
+name=gets.strip
+#ask the user for their name. and store their name in a variable called name
+puts "Hello #{name}!"
